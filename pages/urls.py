9fc3d6b53c5be48516fe9f56a34b6d739379635a
Vit Safarik <vit@safarik.dev>
2020-11-20 21:02:28 +0100

    index, about and linking

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,7 @@ from django.urls.resolvers import URLPattern
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+]
