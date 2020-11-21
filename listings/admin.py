c3b172a65849ddcd5d8d3ddb8fa33ed0c66ded16
Vit Safarik <vit@safarik.dev>
2020-11-21 10:18:46 +0100

    listing, realtors admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Listing
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published", "price")
+
+
+admin.site.register(Listing, ListingAdmin)
