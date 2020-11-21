4855028654bc191781829e015b242099e2408455
Vit Safarik <vit@safarik.dev>
2020-11-21 15:37:51 +0100

    refactor

--- pages/views.py
+++ pages/views.py
@@ -1,25 +1,33 @@
-from realtors.models import Realtor
-from listings.views import listing
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 from listings.models import Listing
 from realtors.models import Realtor
 
-
 def index(request):
-    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
-    contex = {"listings": listings}
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
 
-    return render(request, "pages/index.html", contex)
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    realtors = Realtor.objects.order_by("-hire_date")
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
 
+    # Get MVP
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
 
-    context = {"realtors": realtors, "mvp_realtors": mvp_realtors}
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
 
-    return render(request, "pages/about.html", context)
+    return render(request, 'pages/about.html', context)
