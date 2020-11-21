1f8d80a5995f7b7ca9d15a54f21382b57fb657a8
Vit Safarik <vit@safarik.dev>
2020-11-21 14:21:17 +0100

    home and about

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,25 @@
+from realtors.models import Realtor
+from listings.views import listing
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, "pages/index.html")
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+
+    contex = {"listings": listings}
+
+    return render(request, "pages/index.html", contex)
 
 
 def about(request):
-    return render(request, "pages/about.html")
+    realtors = Realtor.objects.order_by("-hire_date")
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {"realtors": realtors, "mvp_realtors": mvp_realtors}
+
+    return render(request, "pages/about.html", context)
